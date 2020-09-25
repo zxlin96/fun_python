3b99588c521f011127bc8b3fa675e77ef8148dbc
linzhanxiang <linzhanxiang@star-net.cn>
2020-09-25 18:20:59 +0800

    [DEV][ADD][小猫][INIT] 具体描述:         1. 添加小猫出事脚本

---
+++ xmall/auto_buy_fm2_3.py
@@ -0,0 +1,106 @@
+#python3.6.5
+#coding:utf-8
+
+'''
+@author:抢口罩定制
+@time:2020-04-27
+程序利用自动测试工具模拟用户下单操作，完成商品的抢购
+仅作为学习过程中的实践，无商业用途
+'''
+
+from selenium import webdriver
+import datetime
+import time
+from timer import Timer
+
+#创建浏览器对象
+options = webdriver.ChromeOptions()
+options.add_argument('user-agent="5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"')
+driver = webdriver.Chrome('./chromedriver', chrome_options=options)
+
+#窗口最大化显示
+# driver.maximize_window()
+
+def login(url,mall):
+    '''
+    登陆函数
+    
+    url:商品的链接
+    mall：商城类别
+    '''
+    print("请登录")
+    driver.get(url)
+    driver.implicitly_wait(10)
+    time.sleep(2)
+    #淘宝和天猫的登陆链接文字不同
+    print("mall2:",mall==3)
+    if mall==1:
+        #找到并点击淘宝的登陆按钮
+        driver.find_element_by_link_text("亲，请登录").click()
+    elif mall==3:
+        print("30秒后开始自动抢购")
+        time.sleep(30)
+    else:
+        #找到并点击天猫的登陆按钮
+        driver.find_element_by_link_text("请登录").click()
+    #用户扫码登陆
+    if mall!=3:
+        print("30秒后开始自动抢购")
+        time.sleep(30)
+    
+def buy(buy_time,mall):
+    '''
+    购买函数
+    
+    buy_time:购买时间
+    mall:商城类别
+    
+    获取页面元素的方法有很多，获取得快速准确又是程序的关键
+    在写代码的时候运行测试了很多次，css_selector的方式表现最佳
+    '''
+    print("正在抢购，请等待...")
+
+    if mall==1:
+        #"立即购买"的css_selector
+        btn_buy='#J_juValid > div.tb-btn-buy > a'
+        #"立即下单"的css_selector
+        btn_order='#submitOrder_1 > div.wrapper > a'
+    elif mall==3:
+        btn_buy="结 算"
+        btn_order="提交订单"
+    else:
+        btn_buy='#J_LinkBuy'
+        btn_order='#submitOrderPC_1 > div > a'
+    t = Timer(buy_time=buy_time)
+    t.start()
+    if mall==3:
+        #找到“结算按钮”，点击
+        driver.find_element_by_link_text(btn_buy).click()
+    else:
+        #找到“立即购买”，点击
+        driver.find_element_by_css_selector(btn_buy).click()
+
+    while True:
+        try:
+            if mall==3:
+                #找到“提交订单”，点击
+                driver.find_element_by_link_text(btn_order).click()
+            else:
+                driver.find_element_by_css_selector(btn_order).click()
+            #下单成功，跳转至支付页面
+            print("订单提交成功")
+            break
+        except Exception as ee:
+            print(str(ee))
+            time.sleep(0.02)
+    
+
+if __name__ == "__main__":
+    #url = "https://detail.tmall.com/item.htm?spm=a1z0d.6639537.1997196601.4.b42a7484vqMOeS&id=523741145509"
+    url="https://cart.tmall.com/cart.htm?from=bmini&tpId=725677994"
+    mall=int(input("请选择商城（淘宝 1  天猫 2  天猫超市 3输入数字即可）： "))
+    buydate = datetime.date.today()
+    print("mall %d", mall)
+    login(url,mall)
+    buy('2020-09-23 8:39:59.990',mall)
+        
