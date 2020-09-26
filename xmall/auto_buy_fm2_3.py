8151cdabfef6d915639e77fe817d8d79ecca1038
zxlin <848282275@qq.com>
2020-09-26 15:40:13 +0800

    [DEV][MOD][XMALL][多实例+线程定时] 具体描述:         添加了多实例的运行方式         采用线程的定时方式, 更为精确

--- xmall/auto_buy_fm2_3.py
+++ xmall/auto_buy_fm2_3.py
@@ -12,43 +12,48 @@ from selenium import webdriver
 import datetime
 import time
 from timer import Timer
+from datetime import date
+from log import logger
+from datetime import datetime
+import threading
 
 #创建浏览器对象
 options = webdriver.ChromeOptions()
 options.add_argument('user-agent="5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36"')
-driver = webdriver.Chrome('./chromedriver', chrome_options=options)
+# driver = webdriver.Chrome('./chromedriver', chrome_options=options)
+driver_path='../tools/chromedriver'
 
 #窗口最大化显示
 # driver.maximize_window()
 
-def login(url,mall):
+def login(url,mall,driver):
     '''
     登陆函数
     
     url:商品的链接
     mall：商城类别
     '''
-    print("请登录")
+    logger.info("请登录")
     driver.get(url)
     driver.implicitly_wait(10)
     time.sleep(2)
     #淘宝和天猫的登陆链接文字不同
-    print("mall2:",mall==3)
+    logger.info("mall2: %d", mall)
     if mall==1:
         #找到并点击淘宝的登陆按钮
         driver.find_element_by_link_text("亲，请登录").click()
     elif mall==3:
-        print("30秒后开始自动抢购")
+        logger.info("30秒后开始自动抢购")
         time.sleep(30)
     else:
         #找到并点击天猫的登陆按钮
         driver.find_element_by_link_text("请登录").click()
     #用户扫码登陆
     if mall!=3:
-        print("30秒后开始自动抢购")
+        logger.info("30秒后开始自动抢购")
         time.sleep(30)
     
-def buy(buy_time,mall):
+def buy(buy_time,mall,driver):
     '''
     购买函数
     
@@ -58,7 +63,7 @@ def buy(buy_time,mall):
     获取页面元素的方法有很多，获取得快速准确又是程序的关键
     在写代码的时候运行测试了很多次，css_selector的方式表现最佳
     '''
-    print("正在抢购，请等待...")
+    logger.info("正在抢购，请等待...")
 
     if mall==1:
         #"立即购买"的css_selector
@@ -71,8 +76,7 @@ def buy(buy_time,mall):
     else:
         btn_buy='#J_LinkBuy'
         btn_order='#submitOrderPC_1 > div > a'
-    t = Timer(buy_time=buy_time)
-    t.start()
+
     if mall==3:
         #找到“结算按钮”，点击
         driver.find_element_by_link_text(btn_buy).click()
@@ -88,19 +92,42 @@ def buy(buy_time,mall):
             else:
                 driver.find_element_by_css_selector(btn_order).click()
             #下单成功，跳转至支付页面
-            print("订单提交成功")
+            logger.info("订单提交成功")
             break
         except Exception as ee:
-            print(str(ee))
-            time.sleep(0.02)
+            logger.info(str(ee))
+            time.sleep(0.01)
     
 
 if __name__ == "__main__":
     #url = "https://detail.tmall.com/item.htm?spm=a1z0d.6639537.1997196601.4.b42a7484vqMOeS&id=523741145509"
     url="https://cart.tmall.com/cart.htm?from=bmini&tpId=725677994"
     mall=int(input("请选择商城（淘宝 1  天猫 2  天猫超市 3输入数字即可）： "))
-    buydate = datetime.date.today()
-    print("mall %d", mall)
-    login(url,mall)
-    buy('2020-09-23 8:39:59.990',mall)
+    buydate = date.today()
+    logger.info("mall %d", mall)
+    buydate = date.today()
+    buytime = input("请输入开售时间【默认今天 20:00:00.000】")
+    buytime=buytime.strip()
+    if buytime=="".strip():
+        bt = str(buydate) + " 20:00:00.000"
+    else:
+        bt = str(buydate) + " " + buytime
+    logger.info("选定的抢购时间：%s"%(bt))
+    user_count = input("请输入所要抢购的人数 【默认为 1】")
+    if user_count=="".strip():
+        uc = 1
+    else:
+        uc = int(user_count)
+    logger.info("选定的抢购人数：%d"%(uc))
+    
+    i = 0
+    bt_2 = datetime.strptime(bt, "%Y-%m-%d %H:%M:%S.%f")
+    prepare_time = int(time.mktime(bt_2.timetuple()))
+    while i <  uc:
+        driver = webdriver.Chrome(driver_path, chrome_options=options)
+        login(url,mall,driver)
+        # threading.Thread(target=buy, args=(url, bt,driver)).start()
+        threading.Timer(prepare_time-time.time(), function=buy, args=(bt,mall,driver)).start()
+        # timer_test()
+        i=i+1
         
